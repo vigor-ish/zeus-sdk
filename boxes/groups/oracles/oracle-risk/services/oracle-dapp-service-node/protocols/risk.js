9be96f1b57f3bab50eaafaa0e2740b1b3d6cb303
prc <prcolaco@gmail.com>
2019-11-22 11:57:59 +0000

    Risk oracle test implementation calling RiskJS

--- boxes/groups/oracles/oracle-risk/services/oracle-dapp-service-node/protocols/risk.js
+++ boxes/groups/oracles/oracle-risk/services/oracle-dapp-service-node/protocols/risk.js
@@ -1,31 +1,59 @@
 let riskjs;
 
-module.exports = async({ proto, address }) => {
-  // risk://algo/func/arg;arg;...
+// Algorithms
+const ALGORITHM_CVAR = 'cvar';
+
+// CVaR Methods
+const CVAR_METHODS = {
+  montecarlo: RiskJS.portfolioMonteCarloVaR,
+  varcov: () => 'not implemented yet'
+}
+
+module.exports = async ({ proto, address }) => {
+  // risk://algorithm/method/arg;arg;...
   if (!riskjs)
     riskjs = require("riskjs")();
 
+  // Parse the address
+  const [algorithm, method, rest] = address.split('/');
+  const args = rest.split(';');
+
   return new Promise((resolve, reject) => {
-    const fen = address;
-    // console.log("address", address);
-    riskjs.onmessage = function onmessage(event) {
-      if (!event)
-        return;
-      console.log(event);
-      const parts = event.split(' ');
-      let idx = 0;
-      const cmd = parts[idx++];
-      if (cmd == 'bestmove') {
-        const move = parts[idx++];
-        console.log('moveObj', move);
-        resolve(Buffer.from(move));
-      }
+    console.log("address", address);
 
+    // Get price data
+    const data = [];
 
+    // Select the algorithm function
+    const riskCalc = riskCalcFunction(algorithm, method);
+    if (result === undefined) {
+      reject('invalid algorithm/method pair: ' + algorithm + '/' + method);
+      return;
+    }
 
-    };
-    riskjs.postMessage(`uci`);
-    riskjs.postMessage(`position fen ${fen}`);
-    riskjs.postMessage(`go depth 10`);
+    // TODO: what to do with the args (parameters)?
+
+    try {
+      // Call the RiskJS lib
+      const result = riskCalc(data);
+
+      resolve(Buffer.from(result));
+    }
+    catch (e) {
+      reject('calculation failed, reason: ' + e.message);
+    }
   });
 };
+
+// Helpers
+
+// Returns the correct RiskJS function to call based on the definitions in the
+// constants at the top
+function riskCalcFunction(algorithm, method) {
+  switch (algorithm) {
+    case ALGORITHM_CVAR:
+      return CVAR_METHODS[method];
+  }
+
+  return undefined;
+}
